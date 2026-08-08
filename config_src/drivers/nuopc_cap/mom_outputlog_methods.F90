a9c8d4aa6421c3411be70944b4e5b2713eef3d16
denise.worthen <Denise.Worthen@noaa.gov>
2026-08-08 12:20:13 -0600

    fix prefix string to allow underscore; fix setprefix to append _

--- config_src/drivers/nuopc_cap/mom_outputlog_methods.F90
+++ config_src/drivers/nuopc_cap/mom_outputlog_methods.F90
@@ -20,7 +20,7 @@ type :: outputlog_config_type
   integer                 :: opt_n
   logical                 :: requested
   character(len=7)        :: timereduce
-  character(len=12)       :: fnameprefix
+  character(len=13)       :: fnameprefix
   character(len=4)        :: fnamesuffix
   type(ESMF_Alarm)        :: alarm
   type(ESMF_TimeInterval) :: logname_fhoffset
@@ -339,7 +339,7 @@ end function settype
 !! @param[in]   nml_fnameprefix  requested filename prefixes from namelist
 !! @param[out]  errmsg           error message
 !! @param[out]  ierr             return code
-!! @return                       filename prefixes by supported frequency slot
+!! @return                       filename prefixes, underscore added, by supported frequency slot
 function setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr) result(fileprefixes)
 
   integer,          intent(in)  :: validfreqs(:)
@@ -350,8 +350,8 @@ function setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr)
   integer,          intent(out) :: ierr
 
   integer :: n, m, nfreq, n_active
-  character(len=12) :: reqval
-  character(len=12) :: fileprefixes(size(validfreqs))
+  character(len=13) :: reqval
+  character(len=13) :: fileprefixes(size(validfreqs))
 
   nfreq = size(validfreqs)
   ierr = 0
@@ -403,7 +403,7 @@ function setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr)
     if (reqval == '') then
       fileprefixes(n) = 'ocn_'
     else
-      fileprefixes(n) = reqval//'_'
+      fileprefixes(n) = trim(reqval)//'_'
     endif
     return
   endif
@@ -428,7 +428,7 @@ function setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr)
              " but nml_fnameprefix is missing for frequency ", validfreqs(n), "h."
         return
       endif
-      fileprefixes(n) = reqval//'_'
+      fileprefixes(n) = trim(reqval)//'_'
     enddo
 
     ! multi-freq output: must provide unique fileprefixes
